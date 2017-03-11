d523a6cb84ec3e1c63764e9a298f9a1182fbf641
Kendrick Tan <kendricktan0814@gmail.com>
2017-03-10 18:38:07 -0800

    .

--- app/Main.hs
+++ app/Main.hs
@@ -6,7 +6,10 @@ module Main where
 import GHC.Generics
 import Data.Monoid ((<>))
 import Control.Applicative
+import Control.Monad
+import Control.Monad.IO.Class (MonadIO, liftIO)
 
+import qualified Data.Text                          as T
 import qualified Database.SQLite.Simple             as SS
 import qualified Database.SQLite.Simple.FromRow     as SF
 import qualified Data.Aeson                         as DA
@@ -19,12 +22,17 @@ data DeviceLog = DeviceLog { deviceId :: String, epoch :: Int } deriving (Show, 
 instance DA.ToJSON DeviceLog
 instance DA.FromJSON DeviceLog
 
+-- Database access
+fetchDevices :: MonadIO m => SS.Connection -> String -> m [DeviceLog]
+fetchDevices conn xs = return [DeviceLog xs 10]
+
 -- High level routing
-routes :: WS.ScottyM ()
-routes = do
+routes :: SS.Connection -> WS.ScottyM ()
+routes conn = do
     WS.get "/:uid/:date" $ do
-        date <- WS.param "date"
-        WS.text (date <> " !")
+        --date <- (show <$> WS.param "date") :: String
+        tt <- fetchDevices conn "hello"
+        WS.json tt
     WS.get "/:uid/:ftime/:ttime" $ do
         uid   <- WS.param "uid"
         ftime <- WS.param "ftime"
@@ -36,4 +44,7 @@ routes = do
 main :: IO ()
 main = do
     conn <- SS.open "database.db"
-    WS.scotty 8000 routes
+    SS.execute_ conn "CREATE TABLE IF NOT EXISTS devices (id INTEGER PRIMARY KEY, uid TEXT, epoch INTEGER)"
+    --r <- SS.query_ conn "SELECT uid, epoch from devices" :: IO [DeviceLog]
+    --mapM_ print r
+    WS.scotty 8000 $ routes conn
